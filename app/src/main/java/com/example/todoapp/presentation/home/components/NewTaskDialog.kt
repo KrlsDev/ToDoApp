ed9d71733d15041201804824ad6e0a5c5b6b50c6
Karlos <car.mor.delgado@gmail.com>
2025-10-06 19:47:32 +0200

    feature: autofocus text field NewTaskDialog

--- app/src/main/java/com/example/todoapp/presentation/home/components/NewTaskDialog.kt
+++ app/src/main/java/com/example/todoapp/presentation/home/components/NewTaskDialog.kt
@@ -18,9 +18,13 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
@@ -35,6 +39,12 @@ fun NewTaskDialog(
     onDismiss: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 
     Column(
         verticalArrangement = Arrangement.Center,
@@ -82,6 +92,7 @@ fun NewTaskDialog(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(horizontal = 15.dp)
+                    .focusRequester(focusRequester)
             )
             Spacer(Modifier.padding(10.dp))
         }
